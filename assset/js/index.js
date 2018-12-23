6cb138ac00c622499f564af854a34a77d23dffb2
AllenbLLB <18252582787@163.com>
2018-12-23 20:23:18 +0800

    add

---
+++ assset/js/index.js
@@ -0,0 +1,23 @@
+function Wcan(){
+    this.init();
+}
+
+Wcan.prototype.init = function(){
+    this.initPramas();
+    this.initCanvas();
+};
+Wcan.prototype.initPramas = function(){
+    this.$win = $('#wcan');
+    this.$can = this.$win.find('#can');
+    this.ctx = this.$can[0].getContext('2d');
+    this.wcanImg = $('#wcan-img')[0];
+};
+Wcan.prototype.initCanvas = function(){
+    this.$can[0].height = this.$win.height();
+    this.$can[0].width = this.$win.width();
+};
+
+
+$(document).ready(function(){
+    window.wcan = new Wcan();
+});
