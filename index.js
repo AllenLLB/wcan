a3343b0f8aecc5fddc4bd2a006f228fc29ec6eab
AllenbLLB <18252582787@163.com>
2018-12-23 16:21:43 +0800

    add

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+function Wcan(){
+    this.init();
+}
+
+Wcan.prototype.init = function(){
+    this.initPramas();
+    this.initCanvas();
+};
+Wcan.prototype.initPramas = function(){
+    this.$win = $('#wcan');
+    this.$can = this.$win.find('#can');
+    this.ctx = this.$can[0].getContext('2d');
+};
+Wcan.prototype.initCanvas = function(){
+    console.log(this.$can, this.ctx);
+};
+
+
+
 $(document).ready(function(){
-    alert(222);
+    window.wcan = new Wcan();
 });
